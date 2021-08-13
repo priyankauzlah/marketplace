fc762aec36e96bc5317b2fb41c86bbaa74527611
priyanka uzlah <priyankauzlah@gmail.com>
2021-08-13 15:16:18 +0700

    adding button cart

---
+++ lib/model/cart.dart
@@ -0,0 +1,6 @@
+class Cart{
+  int bikeId;
+  Cart({required this.bikeId});
+}
+
+final List<Cart> cartList =[];
--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:marketplace/model/bicycle.dart';
+import 'package:marketplace/model/cart.dart';
+import 'package:marketplace/util/badge.dart';
 import 'package:marketplace/util/widget.dart';
 
 class DetailScreen extends StatefulWidget {
@@ -15,10 +17,8 @@ class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
+        body: SingleChildScrollView(
+          child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
             Stack(
               children: [
                 Container(
@@ -36,24 +36,22 @@ class _DetailScreenState extends State<DetailScreen> {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         IconButton(
-                          icon: Icon(
-                            Icons.arrow_back_ios,
-                            color: Colors.black,
-                          ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
+                            icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+                            onPressed: () {
+                              Navigator.pop(context);}
                         ),
                         Stack(
                           children: [
                             IconButton(
-                                icon: Icon(
-                                  Icons.shopping_cart_outlined,
-                                  color: Colors.black,
-                                ),
-                                onPressed: () {})
+                                onPressed: () {},
+                                icon: Icon(Icons.shopping_cart_outlined,
+                                    color: Colors.black)
+                            ),
+                            cartList.length > 0
+                                ? Badge(numberValue: (cartList.length).toString())
+                                : Container()
                           ],
-                        )
+                        ),
                       ],
                     ),
                   ),
@@ -61,14 +59,15 @@ class _DetailScreenState extends State<DetailScreen> {
               ],
             ),
             Container(
-              transform: Matrix4.translationValues(0.0, -30.0, 0.0),
+              transform: Matrix4.translationValues(0.0, -15.0, 0.0),
               child: Card(
                 elevation: 0,
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                )),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20)
+                  ),
+                ),
                 child: Padding(
                   padding: EdgeInsets.symmetric(vertical: 24, horizontal: 16),
                   child: Column(
@@ -92,34 +91,62 @@ class _DetailScreenState extends State<DetailScreen> {
                                 margin: EdgeInsets.only(top: 4),
                                 child: Text(
                                   widget.bicycle.bikeBrand,
-                                  style: TextStyle(fontWeight: FontWeight.w300),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w300,),
                                 ),
-                              )
+                              ),
                             ],
                           ),
-                          Text(
-                            rupiahCurrencyFormat
-                                .format(widget.bicycle.bikePrice),
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          )
+                          Text(rupiahCurrencyFormat.format(widget.bicycle.bikePrice),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16)
+                          ),
                         ],
                       ),
                       SizedBox(height: 20),
                       Text('Description'),
+                      SizedBox(height: 8),
                       Text(
                         widget.bicycle.bikeDescription,
                         textAlign: TextAlign.justify,
                         style: TextStyle(fontWeight: FontWeight.w300),
-                      )
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 30),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            ElevatedButton(
+                                onPressed: (){},
+                                style: ElevatedButton.styleFrom(
+                                    padding: EdgeInsets.all(14.0),
+                                    primary: Color.fromARGB(0, 255, 255, 255)),
+                                child: Icon(Icons.add_shopping_cart_outlined)
+                            ),
+                            Expanded(
+                              child: Container(
+                                margin: EdgeInsets.only(left: 15),
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      primary: Color.fromARGB(1000, 4, 93, 42),
+                                      padding: EdgeInsets.all(16.0)),
+                                  onPressed: (){},
+                                  child: Text(
+                                      'Buy Now',
+                                      style: TextStyle(color: Colors.white)),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ),
-            )
-          ],
-        ),
-      ),
-    );
+            ),
+          ]),
+        ));
   }
 }
